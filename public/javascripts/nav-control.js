1b1ac50c99f24fb6119f76a0d58e243a1ae4ddb4
Bricey <brice.hall@rogers.com>
2017-07-07 19:53:40 -0400

    Friday evening updates

--- public/javascripts/nav-control.js
+++ public/javascripts/nav-control.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	var targetMenu = $('.navCont a.btn');
-	var chapterLink = $('#chapters .menuCont a');
 
 	targetMenu.on('mouseover',function() {
 		console.log($(this).children('img').addClass('hover'));
@@ -14,21 +13,11 @@ $(document).ready(function() {
 		if ($(this).hasClass('menu')) {
 			$('#chapters').hide();
 			$('header').toggleClass('menuOpen');
-
-		} else if ($(this).hasClass('chapters')) {
-			$('#chapters').show();
-			$('header').toggleClass('chaptersOpen');
 		}
 
 		if ($('body').hasClass('dis-slideout')) {
 			$('body').removeClass('dis-slideout');
 			$('#slideout').removeClass('active');
 		}
-	});
-
-	chapterLink.on('click',function(e) {
-		$('body').toggleClass('dis');
-		$('#progressBar').toggleClass('dis');
-		$('header').removeClass('chaptersOpen');
 	});
 });
