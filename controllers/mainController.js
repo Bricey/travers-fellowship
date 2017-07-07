57cd04c138556e766d8989f5cb47963de37b6012
Bricey <brice.hall@rogers.com>
2017-07-07 11:01:14 -0400

    Fix to package.json

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -6,6 +6,22 @@ exports.mainStory = (req,res) => {
 	res.render('feature',{title:'Syria\'s Refugees'});
 }
 
-exports.profiles = (req,res) => {
-	res.render('profiles',{title:'Profiles'});
+exports.profKhalil = (req,res) => {
+	res.render('khalil',{title:'Profile'});
+}
+
+exports.profFatima = (req,res) => {
+	res.render('fatima',{title:'Profile'});
+}
+
+exports.profMohammed = (req,res) => {
+	res.render('mohammed',{title:'Profile'});
+}
+
+exports.profWadad = (req,res) => {
+	res.render('wadad',{title:'Profile'});
+}
+
+exports.profAbid = (req,res) => {
+	res.render('abid',{title:'Profile'});
 }
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.mainStory);
 router.get('/feature',mainController.mainStory);
-router.get('/khalil',mainController.profiles);
-router.get('/fatima',mainController.profiles);
-router.get('/mohammed',mainController.profiles);
-router.get('/wadad',mainController.profiles);
-router.get('/abid',mainController.profiles);
+router.get('/khalil',mainController.profKhalil);
+router.get('/fatima',mainController.profFatima);
+router.get('/mohammed',mainController.profMohammed);
+router.get('/wadad',mainController.profWadad);
+router.get('/abid',mainController.profAbid);
 
 module.exports = router;
