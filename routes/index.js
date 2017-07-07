223114b5c411f23e07595876c455f1885d5d1783
Bricey <brice.hall@rogers.com>
2017-07-07 10:11:29 -0400

    Fourth commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.mainStory);
 router.get('/feature',mainController.mainStory);
-router.get('/profiles',mainController.profiles);
+router.get('/khalil',mainController.profiles);
+router.get('/fatima',mainController.profiles);
+router.get('/mohammed',mainController.profiles);
+router.get('/wadad',mainController.profiles);
+router.get('/abid',mainController.profiles);
 
 module.exports = router;
